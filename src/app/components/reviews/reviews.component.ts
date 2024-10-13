777a0e9dfb8419e01024a38cf4e0c5f5ab959e8b
Nada628 <n.mahmoud@ecmpp.com>
2024-10-13 16:45:52 +0300

    update

--- src/app/components/reviews/reviews.component.ts
+++ src/app/components/reviews/reviews.component.ts
@@ -1,15 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-reviews',
   templateUrl: './reviews.component.html',
   styleUrls: ['./reviews.component.css']
 })
-export class ReviewsComponent {
+export class ReviewsComponent implements OnInit {
   currentSlide = 0;
   totalSlides = 6; 
   visibleSlides = 3; 
   dots = new Array(this.totalSlides / this.visibleSlides).fill(0); // لعدد النقاط
+
+  ngOnInit() {
+    this.checkScreenSize(); // Set the visible slides based on screen size on load
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenSize(); // Adjust slides on screen resize
+  }
+
+  checkScreenSize() {
+    if (window.innerWidth <= 768) {
+      this.visibleSlides = 1; // Show only 1 slide on mobile
+    } else {
+      this.visibleSlides = 3; // Default to 3 slides on larger screens
+    }
+    this.updateSlider();
+  }
 
   prevSlide() {
     this.currentSlide = (this.currentSlide === 0) ? this.totalSlides - this.visibleSlides : this.currentSlide - 1;
